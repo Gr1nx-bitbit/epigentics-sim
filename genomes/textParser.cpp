6e0a039d82f143d292e3bbf8d375a82d1c6d0603
Gr1nx <gr1nx614@gmail.com>
2024-04-06 22:29:54 -0700

    added current text parser

---
+++ genomes/textParser.cpp
@@ -0,0 +1,56 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include "AminoCodon.h"
+using namespace std;
+
+AminoCodon parse(string input);
+
+int main(int argC, char* argV[]) {
+    //instead of hardcode the path we can pass it in as a CLI argument
+    //cout << argV[1] << endl;
+    string text;
+    vector<AminoCodon> output;
+    ifstream myFile("aminoCodon.txt");
+
+    while(getline(myFile, text, '\n')) {
+        AminoCodon tmp = parse(text);
+        output.push_back(tmp);
+    }
+
+    for (int i = 0; i < output.size(); i++) {
+        if (output[i].startCodon) { 
+            cout << "Start amino acid: " << output[i].aminoAcid << endl;
+        } else if (output[i].terminationCodon) {
+            cout << "Termination acid: " << output[i].aminoAcid << endl;
+        }
+    }
+
+    return 0;
+}
+
+AminoCodon parse(string input) {
+    bool colon = false;
+    AminoCodon tmp;
+    tmp.startCodon = false;
+    tmp.terminationCodon = false;
+    for (int index = 0; index < input.length(); index++) {
+        if (input[index] == ':') {
+            colon = true;            
+        } else if (!colon && input[index] != ' ') {
+            tmp.codon += input[index];
+        } else {
+            if (input[index] == '!') {
+                tmp.startCodon = true;
+                tmp.terminationCodon = false;
+            } else if (input[index] == '/') {
+                tmp.terminationCodon = true;
+                tmp.startCodon = false;
+            } else {
+                tmp.aminoAcid += input[index];
+            }
+        }
+    }
+
+    return tmp;
+}
