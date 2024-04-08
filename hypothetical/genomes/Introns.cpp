fcdefa39e24ace57f81b0dde0ed6af1b3592d752
Gr1nx <gr1nx614@gmail.com>
2024-04-07 17:15:20 -0700

    l

--- hypothetical/genomes/Introns.cpp
+++ hypothetical/genomes/Introns.cpp
@@ -140,7 +140,7 @@ Node* examine(string sequence, int length) {
 
     for (int i = 0; i < length; i++)
     {
-        if (!start && (sequence[i] == 'g') && (sequence[i+1] == 'u') && (i < (length - 1)) && (i != genCur.endIndex)) { //hey
+        if (!start && (sequence[i] == 'g') && (sequence[i+1] == 'u') && (i < (length - 1)) && (i != genCur.endIndex)) {
             start = true;
             genCur.startIndex = i;
         } else if (start && (sequence[i] == 'a') && (sequence[i+1] == 'g') && (i < (length - 1)) && (i != genCur.startIndex)) {
