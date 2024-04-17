153c92c2a06b3cae58239700b6aecf06cd0dca73
Gr1nx <gr1nx614@gmail.com>
2024-04-16 21:50:47 -0700

    build folder so you don't have to deal with all the extra files

--- cmake/build/CMakeFiles/Makefile.cmake
+++ cmake/build/CMakeFiles/Makefile.cmake
@@ -7,10 +7,10 @@ set(CMAKE_DEPENDS_GENERATOR "Unix Makefiles")
 # The top level Makefile was generated from the following files:
 set(CMAKE_MAKEFILE_DEPENDS
   "CMakeCache.txt"
+  "/Users/reaper/workspace/DNA/cmake/CMakeLists.txt"
   "CMakeFiles/3.29.2/CMakeCCompiler.cmake"
   "CMakeFiles/3.29.2/CMakeCXXCompiler.cmake"
   "CMakeFiles/3.29.2/CMakeSystem.cmake"
-  "CMakeLists.txt"
   "/opt/homebrew/Cellar/cmake/3.29.2/share/cmake/Modules/CMakeCCompiler.cmake.in"
   "/opt/homebrew/Cellar/cmake/3.29.2/share/cmake/Modules/CMakeCCompilerABI.c"
   "/opt/homebrew/Cellar/cmake/3.29.2/share/cmake/Modules/CMakeCInformation.cmake"
